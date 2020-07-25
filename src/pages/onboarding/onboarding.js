e8f74fb447904a99c3e3ce88f4f68e65be86f86b
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2020-07-25 13:41:42 +0530

    style fixes

--- src/pages/onboarding/onboarding.js
+++ src/pages/onboarding/onboarding.js
@@ -8,15 +8,15 @@ function Onboarding() {
 			title={`Let's get you onboard`}
 			description="Setup live streaming in minutes"
 		>
-			<div className={styles.onboardingFormContainer}>
-				<iframe
-					className={styles.onboardingForm}
-					src="https://docs.google.com/forms/d/e/1FAIpQLSfCg9C7FGJk7hv8m5BfJTTfhdlF8QslCytZ47Ro2ZVPO2wGiQ/viewform?embedded=true"
-					frameborder="0"
-					marginheight="0"
-					marginwidth="0"
-				/>
-			</div>
+			{/* <div className={styles.onboardingFormContainer}> */}
+			<iframe
+				className={styles.onboardingForm}
+				src="https://docs.google.com/forms/d/e/1FAIpQLSfCg9C7FGJk7hv8m5BfJTTfhdlF8QslCytZ47Ro2ZVPO2wGiQ/viewform?embedded=true"
+				frameborder="0"
+				marginheight="0"
+				marginwidth="0"
+			/>
+			{/* </div> */}
 		</Layout>
 	);
 }
